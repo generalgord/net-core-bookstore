9bf4df88c5bd7fb4431cf6219abb97773d63753d
Serdar Ergün <generalgord@gmail.com>
2022-10-30 21:23:11 +0300

    Added some validation changes

--- WebApi/Operations/BookOperations/Commands/Create_BookCommandValidator.cs
+++ WebApi/Operations/BookOperations/Commands/Create_BookCommandValidator.cs
@@ -6,9 +6,9 @@ namespace WebApi.Operations.BookOperations.Commands
     {
         public CreateBookCommandValidator()
         {
-            RuleFor(cmd => cmd.Model.GenreId).GreaterThan(0);
+            RuleFor(cmd => cmd.Model.GenreId).GreaterThan(0).IsInEnum();
             RuleFor(cmd => cmd.Model.PageCount).GreaterThan(0);
-            RuleFor(cmd => cmd.Model.PublishDate).NotEmpty().LessThan(DateTime.Now.Date);
+            RuleFor(cmd => cmd.Model.PublishDate).NotEmpty();
             RuleFor(cmd => cmd.Model.Title).NotEmpty().MinimumLength(4);
         }
     }
